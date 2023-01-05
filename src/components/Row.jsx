a6d8651ccf3fe5a8103862d86a760c94c874ae21
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2023-01-05 19:46:57 +0100

    pixel-art-done

---
+++ src/components/Row.jsx
@@ -0,0 +1,18 @@
+import '../styles/row.scss';
+import Pixel from "./Pixel"
+
+const Row = ({ width, selectedColor }) => {
+
+  let pixels = []
+
+  for(let i = 0; i < width; i++) {
+    pixels.push(<Pixel key={i} selectedColor={selectedColor} />)
+  }
+
+  return (
+    <div className='row'>{pixels}</div>
+  )
+}
+
+export default Row
+ 
